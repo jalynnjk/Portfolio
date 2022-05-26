4766dc7e7dbc6b72a6f3f988af0644c01f69cfe9
git  --version <jalynnking@gmail.com>
2022-05-26 18:27:17 -0400

    Add event listener to work with me button that will open user's default email application

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+//Work with me button will open user's default email program
+let workWithMeBtn = document.querySelector('.work-with-me');
+let sendEmail = function () {
+    window.open('mailto:jalynnking@gmail.com?subject=Regarding your portfolio...&body=`${workWithMeBtn}`');
+}
+workWithMeBtn.addEventListener('click', sendEmail)
