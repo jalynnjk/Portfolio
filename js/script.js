09b8f27f3997d29caee06afc1f2a35f602fee018
git  --version <jalynnking@gmail.com>
2022-05-27 22:02:59 -0400

    Code script for project carousel to switch through pictures when next/prev button is pressed

--- js/script.js
+++ js/script.js
@@ -4,3 +4,35 @@ let sendEmail = function () {
     window.open('mailto:jalynnking@gmail.com?subject=Regarding your portfolio...&body=`${workWithMeBtn}`');
 }
 workWithMeBtn.addEventListener('click', sendEmail)
+
+//Projects Carousel (Small Screens)
+//Indexes
+let currentImageIndex = 0
+let prevImageIndex = 0
+//Query Variables
+let nextBtn = document.querySelector('.next-button')
+let prevBtn = document.querySelector('.prev-button')
+const images = document.querySelectorAll('.carousel-image')
+//Functions
+const nextFunction = () => {
+    prevImageIndex = currentImageIndex
+    if (currentImageIndex === (images.length - 1)) {
+        currentImageIndex = 0
+    } else {
+        currentImageIndex++
+    }
+    images[prevImageIndex].style.display = 'none';
+    images[currentImageIndex].style.display = 'block'
+}
+const prevFunction = () => {
+    prevImageIndex = currentImageIndex;
+    currentImageIndex--
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1
+    }
+    images[currentImageIndex].style.display = 'block'
+    images[prevImageIndex].style.display = 'none'
+}
+//Event Listeners
+nextBtn.addEventListener('click', nextFunction)
+prevBtn.addEventListener('click', prevFunction)
