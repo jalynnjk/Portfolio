f6bc55245aad20e3de1ba10dfd88637738c74c95
git  --version <jalynnking@gmail.com>
2022-05-30 19:16:04 -0400

    Add resume images to resume modal html, link images to resume download source, style in css, use script.js for carousel functioning

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-//Work with me button will open user's default email program//
+//On Click: Work With me Button Opens Default Email Application//
+//Query Variables
 let workWithMeBtns = document.querySelectorAll('.work-with-me');
+//Function and Event Listener
 workWithMeBtns.forEach((btn) => {
-btn.addEventListener('click', () => {
-	window.open(
-		'mailto:jalynnking@gmail.com?subject=Regarding your portfolio...'
-	);
+	btn.addEventListener('click', () => {
+		window.open(
+			'mailto:jalynnking@gmail.com?subject=Regarding your portfolio...'
+		);
+	});
 });
-})
 
-//Projects Carousel//
+//Landing Page: Projects Carousel//
 //Indexes
 let currentImageIndex = 0;
 let prevImageIndex = 0;
@@ -40,7 +42,7 @@ const prevFunction = () => {
 nextBtn.addEventListener('click', nextFunction);
 prevBtn.addEventListener('click', prevFunction);
 
-//Socials nav link into logo links//
+//On Click: Socials Text Turns Into Logo Links//
 //Query Variables
 let socialsText = document.querySelector('.socials');
 let headerLogo = document.getElementsByClassName('header-logo');
@@ -60,39 +62,71 @@ socialsText.addEventListener('click', socialsAway);
 //Query Variables
 let projectsModalButton = document.querySelector('.projects');
 let projectsModal = document.querySelector('.projects-modal');
-let mainEl = document.querySelector('.lpMain');
-let headerEl = document.querySelector('.lpHeader');
+let mainEl = document.querySelector('.lp-main');
+let headerEl = document.querySelector('.lp-header');
 //Functions
 showProjectsModal = (event) => {
 	event.preventDefault();
 	projectsModal.style.display = 'block';
 	mainEl.style.display = 'none';
 	headerEl.style.display = 'none';
-	console.log('hi');
 };
 //Event Listeners
 projectsModalButton.addEventListener('click', showProjectsModal);
 
-//Clock projects modal//
-//Query Variables
-let returnBtn = document.querySelector('.return')
-//Functions
-returnHome = () => {
-    projectsModal.style.display = 'none'
-    mainEl.style.display = 'none'
-    headerEl.style.display = 'none'
-}
-//Event Listeners
-returnBtn.addEventListener('click', returnHome)
-//Get videos to play/pause on projects page when mouse-over/leave events occur//
+//Projects Modal: Play/Pause Videos on Mouseover/Mouseleave//
 //Query Variables
 let videos = document.querySelectorAll('.video');
-// //Event Listeners and function
+//Functions and Event Listeners
 videos.forEach((video) => {
-video.addEventListener('mouseover', () => {
-    video.play()
+	video.addEventListener('mouseover', () => {
+		video.play();
+	});
+	video.addEventListener('mouseleave', () => {
+		video.pause();
+	});
 });
-video.addEventListener('mouseleave', () => {
-    video.pause()
-});
-});
+
+//Open Resume Page as Modal//
+//Query Variables
+let resumeModalBtn = document.querySelector('.resume');
+let resumeModal = document.querySelector('.resume-modal');
+//Functions
+let openResumeModal = (event) => {
+	event.preventDefault()
+	resumeModal.style.display = 'block';
+	mainEl.style.display = 'none';
+	headerEl.style.display = 'none';
+};
+//Event Listeners
+resumeModalBtn.addEventListener('click', openResumeModal);
+
+//Resume Carousel//
+//Indexes declared previously
+//Query Variables
+const resumeImages = document.querySelectorAll('.resume-img')
+let resumePrevBtn = document.querySelector('.resume-prev-btn')
+let resumeNxtBtn = document.querySelector('.resume-next-btn')
+//Functions
+const nextResumeImgFunction = () => {
+	prevImageIndex = currentImageIndex;
+	if (currentImageIndex === resumeImages.length - 1) {
+		currentImageIndex = 0;
+	} else {
+		currentImageIndex++;
+	}
+	resumeImages[prevImageIndex].style.display = 'none';
+	resumeImages[currentImageIndex].style.display = 'block';
+};
+const prevResumeImgFunction = () => {
+	prevImageIndex = currentImageIndex;
+	currentImageIndex--;
+	if (currentImageIndex < 0) {
+		currentImageIndex = resumeImages.length - 1;
+	}
+	resumeImages[currentImageIndex].style.display = 'block';
+	resumeImages[prevImageIndex].style.display = 'none';
+};
+//Event Listener
+resumePrevBtn.addEventListener('click', prevResumeImgFunction)
+resumeNxtBtn.addEventListener('click', nextResumeImgFunction);
